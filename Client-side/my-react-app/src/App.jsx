8753f8ea87080ca6a9a3dd352d50e635ec114683
hisham <hisham4a4d4m@gmail.com>
2026-03-21 23:55:58 +0200

    intial repo

---
+++ Client-side/my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section>
+        <h1 className="text-4xl">Hello World</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
